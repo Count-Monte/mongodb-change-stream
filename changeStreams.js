aeeb21a627629fcd9a03d07a543849de550195f6
Count-Monte <edmund.mcgardner@gmail.com>
2023-06-16 01:28:49 -0400

    first commit

---
+++ changeStreams.js
@@ -0,0 +1,54 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://amazidea0000:123qweQWE@cluster0.gjcgi9y.mongodb.net";
+    
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+     */
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+
+        await monitorListingsUsingEventEmitter(client);
+
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+// Add functions that make DB calls here
+
+function closeChangeStream(timeInMs = 6000, changeStream) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Closing the change stream');
+      resolve(changeStream.close());
+    }, timeInMs);
+  })
+}
+
+async function monitorListingsUsingEventEmitter(client, timeInMs = 60000, pipeline = []){  
+  const collection = client.db("sample_airbnb").collection("listingsAndReviews");
+  const changeStream = collection.watch(pipeline);
+  changeStream.on('change', next => {
+    console.log(next);
+  })
+
+  await closeChangeStream(timeInMs, changeStream);
+}
